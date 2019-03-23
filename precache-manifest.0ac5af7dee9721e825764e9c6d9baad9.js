6074f9c1cd24d22ca1d8b422bc6db2fc2cfc0c04
Rick Love <rick@toldpro.com>
2019-03-23 14:08:54 -0500

    Updates

---
+++ precache-manifest.0ac5af7dee9721e825764e9c6d9baad9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "db2007518b1d6031cc87",
+    "url": "/react-practice-app/static/css/main.80377e6b.chunk.css"
+  },
+  {
+    "revision": "9d303605da5cddef5b3e",
+    "url": "/react-practice-app/static/js/runtime~main.d6b13499.js"
+  },
+  {
+    "revision": "38d77552b0353684a208177482d5b6ee",
+    "url": "/react-practice-app/static/media/primeicons.38d77552.svg"
+  },
+  {
+    "revision": "944f8a5b8984a241b13a",
+    "url": "/react-practice-app/static/js/2.4b3a98ac.chunk.js"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "db2007518b1d6031cc87",
+    "url": "/react-practice-app/static/js/main.826292e4.chunk.js"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/react-practice-app/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/react-practice-app/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/react-practice-app/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "b8eccb1059ea5faaf6d8b7d457ccfd09",
+    "url": "/react-practice-app/static/media/primeicons.b8eccb10.eot"
+  },
+  {
+    "revision": "473e2a746d3c151d7dcaa626a7c84c60",
+    "url": "/react-practice-app/static/media/primeicons.473e2a74.ttf"
+  },
+  {
+    "revision": "71bb3d79dcf18b45ae845409e7c2ada3",
+    "url": "/react-practice-app/static/media/primeicons.71bb3d79.woff"
+  },
+  {
+    "revision": "944f8a5b8984a241b13a",
+    "url": "/react-practice-app/static/css/2.18db4fc7.chunk.css"
+  },
+  {
+    "revision": "c8c1aaafb31fe0a4ff88cd131ebb24c0",
+    "url": "/react-practice-app/index.html"
+  }
+];
